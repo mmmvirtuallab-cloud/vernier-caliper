d2a1509035bac5f7ac0a2fda09a66830615729ba
ismail <ismailisims1@gmail.com>
2025-12-21 00:31:22 +0530

    added intor page *ismail

---
+++ intro/intro_data.js
@@ -0,0 +1,29 @@
+// js/intro_data.js
+
+export const INTRO_CONTENT = {
+    // Top Blue Bar Text
+    headerTitle: "Vernier Caliper",
+    
+    // Button Text & Link
+    buttonLabel: "Go to Experiment",
+    buttonLink: "external.html",
+
+    // Main Page Content
+    mainHeading: "Vernier Caliper - Internal & External Measurement",
+    
+    // Aim Section
+    aimTitle: "Aim",
+    aimContent: "To perform internal and external measurements using Vernier Caliper.",
+
+    // Acknowledgement Section
+    ackTitle: "Acknowledgement",
+    ackContent: "The Department of Production Technology appreciates the following students for their support in the development of Metrology and measurements virtual laboratory experiment.",
+    
+    // Student List
+    students: [
+        "A. M. ISMAIL - 2023507030",
+        "V. B. JAYARAM - 2023507040",
+        "M. RAM BHARATH - 2023507039",
+        "R. MONISH KUMAR - 2023507038"
+    ]
+};
